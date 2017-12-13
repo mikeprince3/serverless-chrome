e93316408b9e0ca73a474844accb7bbb079b2989
Mike Prince <mikep@sagewire.com>
2017-12-13 10:39:24 -0500

    tweak

--- examples/serverless-framework/aws/src/chrome/crawl.js
+++ examples/serverless-framework/aws/src/chrome/crawl.js
@@ -69,7 +69,8 @@ export default async function crawlUrl (url, mobile = false) {
       html: document.documentElement.outerHTML
     })`;
 
-
+    const contentResult = await Runtime.evaluate({expression, returnByValue: true});
+    
     var resultObj = contentResult.result.value;
     
     await Emulation.setDeviceMetricsOverride({
